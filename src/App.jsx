6036947a1ca981054aac97607548b717308e9617
Wiktor Ilczuk <137764780+Wilczuk1506@users.noreply.github.com>
2025-07-23 12:30:30 +0200

    imported the old site to react project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [toggle, setToggle] = useState(true);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Wall paint colours</h1>
+      </header>
+      <nav>
+        <ul>
+          <li><p onClick={() => { setToggle(true) }}>Home</p></li>
+          <li><p onClick={() => { setToggle(false) }}>Recommended colours</p></li>
+        </ul>
+      </nav>
+      {
+        toggle ?
+          <main>
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lobortis, lorem sit amet consectetur sollicitudin, purus neque maximus dolor, a suscipit odio dolor varius dui. In quis laoreet nunc. Proin id risus sed eros molestie luctus. Donec nec gravida ligula. Mauris tincidunt magna massa, eu mollis sapien bibendum sed. Quisque faucibus dapibus ex vitae convallis. Donec erat tellus, dignissim nec felis quis, congue cursus dolor. Curabitur efficitur, leo sed varius porttitor, erat justo tincidunt nulla, non tempor nisl lorem nec odio. Fusce sollicitudin tincidunt ornare. Ut sit amet pulvinar neque. Phasellus nec congue purus. Suspendisse potenti. Sed tellus tellus, pretium in metus non, luctus ornare est. Suspendisse facilisis ornare erat, eget viverra tortor ultricies quis. Cras at consectetur eros, id varius eros.</p>
+            <p>Proin eleifend non lectus sit amet tincidunt. Proin suscipit eu diam et condimentum. Maecenas sit amet neque mi. Nulla varius metus ac nisl posuere, ac mollis ipsum semper. Sed volutpat volutpat eleifend. Mauris tempor nisi et lectus cursus, at laoreet massa placerat. Nullam vehicula sit amet risus ut pulvinar.</p>
+            <p>Sed posuere ornare erat, non pulvinar turpis dignissim ullamcorper. In pretium enim nec fermentum euismod. Nulla ullamcorper aliquam ultricies. Etiam ut mi scelerisque, commodo ante sit amet, sodales lacus. Curabitur dolor mauris, fermentum in urna id, fermentum elementum urna. Sed massa purus, tempor at sagittis pretium, ultrices a sapien. In malesuada ornare dapibus. Etiam non auctor ligula. In lacus nunc, finibus non imperdiet nec, venenatis dapibus quam.</p>
+          </main>
+          :
+          <main>
+            <h2>Our favourite colours</h2>
+            <div className='div'>
+              <img src="images/fluorescent_green__50940.webp" alt="" />
+              <img src="images/fluorescent_lemon__88242.webp" alt="" />
+              <img src="images/fluorescent_pink__88610.webp" alt="" />
+              <img src="images/fluorescent_violet__72879.webp" alt="" />
+            </div>
+          </main>
+      }
     </>
   )
 }
 
-export default App
+export default App;
