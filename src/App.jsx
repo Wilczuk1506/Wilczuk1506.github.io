a91aa205244b43adf36173354b69fe84184f8ee9
Wiktor Ilczuk <137764780+Wilczuk1506@users.noreply.github.com>
2025-08-02 01:44:30 +0200

    added oretty hovers, home is i think done

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,26 @@ const App = () => {
   const [typingDone, setTypingDone] = useState(false);
 
   return (
-    <main className="snap-y snap-mandatory overflow-y-scroll h-screen no-scrollbar text-white font-mono libertinus-sans-regular">
-      <section className="snap-start h-screen bg-gray-950 flex flex-col items-center justify-center">
-        <header className="mb-4 text-center text-7xl leading-relaxed h-60">
+    <main className="h-screen overflow-y-scroll font-mono text-white snap-y snap-mandatory no-scrollbar libertinus-sans-regular">
+      <section className="flex flex-col items-center justify-center h-screen snap-start bg-gray-950">
+        <header className="mb-4 leading-relaxed text-center text-7xl h-60">
           <TypingText onTypingDone={() => setTypingDone(true)}>Hi, Hello, Witam</TypingText>
           <br />
           {typingDone && <RandomFontText>I'm Wilczuk</RandomFontText>}
         </header>
-        <nav className="flex flex-row justify-center border-4 border-gray-900 rounded-full overflow-hidden mx-auto w-[52vw] h-24">
-          <div className="transform skew-x-[20deg] flex items-center justify-center w-[18vw] min-w-[18vw] h-24 hover:bg-black/10 cursor-pointer">
+        <nav className="flex flex-row justify-center border-4 border-gray-800 rounded-full overflow-hidden mx-auto w-[52vw] h-24">
+          <div className="transform skew-x-[20deg] flex items-center justify-center w-[18vw] min-w-[18vw] h-24 hover:bg-gray-800/40 cursor-pointer transition-colors duration-200 ease-in-out">
             <span className="inline-block transform -skew-x-[20deg] text-3xl tracking-widest -translate-y-1 pl-8">About</span>
           </div>
-          <div className="transform skew-x-[20deg] border-l-4 border-r-4 border-gray-900 flex items-center justify-center w-[18vw] min-w-[18vw] h-24 hover:bg-black/10 cursor-pointer">
+          <div className="transform skew-x-[20deg] flex items-center justify-center w-[18vw] min-w-[18vw] h-24 hover:bg-gray-800/40 cursor-pointer transition-colors duration-200 ease-in-out border-l-4 border-r-4 border-gray-800">
             <span className="inline-block transform -skew-x-[20deg] text-3xl tracking-widest -translate-y-1">Projects</span>
           </div>
-          <div className="transform skew-x-[20deg] flex items-center justify-center w-[18vw] min-w-[18vw] h-24 hover:bg-black/10 cursor-pointer">
+          <div className="transform skew-x-[20deg] flex items-center justify-center w-[18vw] min-w-[18vw] h-24 hover:bg-gray-800/40 cursor-pointer transition-colors duration-200 ease-in-out">
             <span className="inline-block transform -skew-x-[20deg] text-3xl tracking-widest -translate-y-1 pr-8">Contact</span>
           </div>
         </nav>
       </section>
-      <section className="snap-start h-screen bg-emerald-500">Section 2</section>
+      <section className="h-screen snap-start bg-emerald-500">Section 2</section>
     </main>
   );
 }
